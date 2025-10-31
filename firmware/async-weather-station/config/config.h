14a2c7ec1db424c0593948ca8dca3dda396ad025
adriancrestita <adrian.morarodri32@gmail.com>
2025-10-31 16:48:37 +0100

    ya conecta wifi, falta sollucionar conexión mqtt

--- firmware/async-weather-station/config/config.h
+++ firmware/async-weather-station/config/config.h
@@ -4,9 +4,9 @@
 #include <IPAddress.h>
 
 // --- WiFi ---
-const char* ssid     = "cubicua";
-const char* password = "";
-const char* hostname = "ESP32_METEO_001";
+const char* ssid     = "DIGI-R5mE";
+const char* password = "hEypg6XW";
+const char* hostname = "";
 
 // Nota: los parámetros IP permanecen solo como referencia; no se utilizan porque la conexión es por DHCP.
 IPAddress ip(192, 168, 1, 200);
@@ -15,7 +15,7 @@ IPAddress subnet(255, 255, 255, 0);
 
 // --- MQTT ---
 // IP de tu broker Mosquitto (formato: 192,168,1,10)
-#define MQTT_HOST       192,168,1,241
+#define MQTT_HOST       "192.168.1.241"
 #define MQTT_PORT       1883
 #define MQTT_USER       ""
 #define MQTT_PASSWORD   ""
--- firmware/async-weather-station/src/async-weather-station/async-weather-station.ino
+++ firmware/async-weather-station/src/async-weather-station/async-weather-station.ino
@@ -18,10 +18,10 @@
 #include <Adafruit_SSD1306.h>
 #include <ArduinoJson.h>
 
-#include "../../ESP32_Utils.hpp"
-#include "../../ESP32_Utils_MQTT_Async.hpp"
-#include "../../MQTT.hpp"
-#include "../../TimeUtils.hpp"
+#include "../../include/ESP32_Utils.hpp"
+#include "../../include/ESP32_Utils_MQTT_Async.hpp"
+#include "../../include/MQTT.hpp"
+#include "../../include/TimeUtils.hpp"
 
 // === Pines ===
 #define DHTPIN 14
